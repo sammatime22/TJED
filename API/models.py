2f18331d05c2243393c91dafa78e6b38688ae497
sammyreles22 <samuel.reles@gmail.com>
2022-07-02 23:26:56 -0400

    API documentation

--- API/models.py
+++ API/models.py
@@ -2,12 +2,25 @@ from django.db import models
 
 
 class Kanji(models.Model):
+    '''
+    Model representing Kanji, as well as attributes associated to said Kanji.
+
+    Parameters:
+    ----------
+    kanji_character : The single character itself
+    meaning : The meaning of the character
+    on_yomi : The Sino-Japanese reading
+    kun_yomi : The native Japanese reading
+    '''
     kanji_character = models.CharField(max_length=1, primary_key=True)
     meaning = models.CharField(max_length=100)
     on_yomi = models.CharField(max_length=100)
     kun_yomi = models.CharField(max_length=100)
 
     def __str__(self):
+        '''
+        Returns a JSON string of the Kanji model.
+        '''
         return str({
             "kanji": self.kanji_character,
             "meaning": self.meaning,
@@ -17,11 +30,24 @@ class Kanji(models.Model):
 
 
 class Vocab(models.Model):
+    '''
+    A model representing both the Japanese and English word, along with assistance in reading the
+    Japanese word if needed (furigana).
+
+    Parameters:
+    ----------
+    japanese_word : The Japanese word associated with the vocab
+    furigana (optional) : An assistance to reading the Japanese word
+    english_word : The English word associated with the vocab
+    '''
     japanese_word = models.CharField(max_length=100, primary_key=True)
     furigana = models.CharField(max_length=100)
     english_word = models.CharField(max_length=100)
 
     def __str__(self):
+        '''
+        Returns a JSON string of the Vocab model.
+        '''
         if self.furigana is not None:
             return str({
                 "japanese_word": self.japanese_word,
--- API/views.py
+++ API/views.py
@@ -11,6 +11,18 @@ EMPTY_RESPONSE = "{}"
 
 
 def get_kanji(request, kanji_character):
+    '''
+    Searches for data associated with a Kanji character.
+
+    Parameters:
+    ----------
+    request : The request from the user
+    kanji_character : The Kanji associated with the request
+
+    Return:
+    ----------
+    A HTTP response, alongside Kanji data, if applicable.
+    '''
     try:
         if len(kanji_character) == EXPECTED_KANJI_QUERY_LENGTH:
             kanji = Kanji.objects.filter(kanji_character__exact=kanji_character)
@@ -28,6 +40,19 @@ def get_kanji(request, kanji_character):
 
 
 def get_vocab_using_japanese(request, japanese_word_query):
+    '''
+    Searches vocabulary, typically for the retrieval of the associated English word,
+    provided the Japanese word.
+
+    Parameters:
+    ----------
+    request : The request from the user
+    japanese_word_query : The Japanese word associated with the request
+
+    Return:
+    ----------
+    A HTTP response, alongside Vocab data, if applicable.
+    '''
     try:
         vocab = Vocab.objects.filter(japanese_word__exact=japanese_word_query)
         if len(vocab) == EXPECTED_NUMBER_OF_RESULTS_FROM_QUERY:
@@ -43,6 +68,19 @@ def get_vocab_using_japanese(request, japanese_word_query):
 
 
 def get_vocab_using_english(request, english_word_query):
+    '''
+    Searches vocabulary, typically for the retrieval of the associated Japanese word,
+    provided the English word.
+
+    Parameters:
+    ----------
+    request : The request from the user
+    english_word_query : The English word associated with the request
+
+    Return:
+    ----------
+    A HTTP response, alongside Vocab data, if applicable.
+    '''
     try:
         vocab = Vocab.objects.filter(english_word__exact=english_word_query)
         if len(vocab) == EXPECTED_NUMBER_OF_RESULTS_FROM_QUERY:
